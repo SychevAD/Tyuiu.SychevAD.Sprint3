61ba7dbd34a05820288c212a2ab76aaac40a9f41
Sychev-Aleksandr <cool.27101978@mail.ru>
2025-09-22 21:48:02 +0500

    change

--- Tyuiu.SychevAD.Sprint3.Task2.V18.Lib/DataService.cs
+++ Tyuiu.SychevAD.Sprint3.Task2.V18.Lib/DataService.cs
@@ -5,8 +5,9 @@ namespace Tyuiu.SychevAD.Sprint3.Task2.V18.Lib
 {
     public class DataService : ISprint3Task2V18
     {
-        public double GetMultiplySeries(double a, int startValue, int stopValue)
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
         {
+            double a = value / 10.0; // Преобразуем int в double (например, 5 -> 0.5)
             double multiplySeries = 1;
             int k = startValue;
 
